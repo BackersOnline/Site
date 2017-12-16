38f8f881fcdc2bd4f427618b50d371333c447c65
Nic Bonetto <nick.bonetto@gmail.com>
2017-12-16 10:26:06 -0800

    Project scaffolding files. #1

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import path from 'path'
+
+dotenv.config()
+
+const app = express()
+
+app.use(express.static(__dirname + '../client/static/public/'))
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/static/public/index.html'))
+})
+app.listen(process.env.PORT)
