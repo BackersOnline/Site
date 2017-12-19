faefd91e5ccb47db6aa360fbef60df48748b4e8e
Nic Bonetto <nick.bonetto@gmail.com>
2017-12-19 15:12:47 -0800

    Created the navigation bar. #5

--- client/components/home/home.jsx
+++ client/components/home/home.jsx
@@ -2,11 +2,13 @@ import React, { Component } from 'react'
 import Intro from './intro-page.jsx'
 import Description from './products-description.jsx'
 import Bitcoin from './bitcoin.jsx'
+import NavBar from '../navbar.jsx'
 
 class HomePage extends Component {
   render() {
     return (
       <section className="container-fluid">
+        <NavBar/>
         <Intro/>
         <Description/>
         <Bitcoin/>
